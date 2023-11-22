320d29a1bac6527ad406056e4ac0163615f3bcc5
Mohamed Thoyyib K <mohamed.k@bytestrone.com>
2023-11-22 19:06:59 +0530

    Added sample console

--- src/craftyBuilder.Application/obj/Debug/net8.0/craftyBuilder.Application.AssemblyInfo.cs
+++ src/craftyBuilder.Application/obj/Debug/net8.0/craftyBuilder.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("craftyBuilder.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08ef93265f86ab1f2b5749ed345124a1705e42e4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5e4656caa49a403479c6e271f8fa893985275e2")]
 [assembly: System.Reflection.AssemblyProductAttribute("craftyBuilder.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("craftyBuilder.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- src/craftyBuilder.UI/Program.cs
+++ src/craftyBuilder.UI/Program.cs
@@ -15,7 +15,53 @@ System.Console.WriteLine("Ai Started inside");
 var app = host.Services.GetRequiredService<ITest>();
 
 app.printSomething();
-host.Run();
+int selectedIndex = 0;
+string[] options = { "Option 1", "Option 2", "Option 3", "Exit" };
+
+while (true)
+{
+  Console.Clear();
+
+  // Display the menu options
+  for (int i = 0; i < options.Length; i++)
+  {
+    if (i == selectedIndex)
+    {
+      Console.BackgroundColor = ConsoleColor.Gray;
+      Console.ForegroundColor = ConsoleColor.Black;
+    }
+
+    Console.WriteLine($"{i + 1}. {options[i]}");
+
+    Console.ResetColor();
+  }
+
+  ConsoleKeyInfo key = Console.ReadKey();
+
+  switch (key.Key)
+  {
+    case ConsoleKey.UpArrow:
+      selectedIndex = (selectedIndex - 1 + options.Length) % options.Length;
+      break;
+    case ConsoleKey.DownArrow:
+      selectedIndex = (selectedIndex + 1) % options.Length;
+      break;
+    case ConsoleKey.Enter:
+      if (selectedIndex == options.Length - 1)
+      {
+        Console.WriteLine("Exiting program...");
+        return;
+      }
+      else
+      {
+        Console.WriteLine($"You selected {options[selectedIndex]}");
+        // Add your logic for the selected option
+      }
+      break;
+  }
+}
+
+// host.Run();
 
 
-System.Console.WriteLine("Ai Started outside");
+// System.Console.WriteLine("Ai Started outside");
